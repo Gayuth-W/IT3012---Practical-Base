af9a52246081bcbee801fec3d98b6fa94909e072
Gayuth Waidyaratne <gayuthdemika@gmail.com>
2026-07-28 20:46:39 +0530

    toxic trap penalty (trap is persistent, it is not consumed)

--- visual_grid_game.py
+++ visual_grid_game.py
@@ -84,6 +84,10 @@ class VisualGridHuntGame:
             self.food_positions.remove(tuple_pos)
             self.score += 20
 
+        # Step 2.3
+        if tuple_pos in self.toxic_traps:
+            self.score -= 15
+
         for op in self.opponents:
             move = random.choice(['Up', 'Down', 'Left', 'Right', 'Stay'])
             if move == 'Up' and op[1] < self.height - 1:
